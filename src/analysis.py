5b1bd7255abe1eba1ec6ad72b67fe711df48d54f
890tsugua <aubhdk@gmail.com>
2026-03-15 16:46:42 +0100

    added plot config

--- src/analysis.py
+++ src/analysis.py
@@ -6,6 +6,9 @@ import json
 import os
 import networkx as nx
 import igraph as ig
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from cycler import cycler
 
 
 def load_starwars_data(file="starwars-full-interactions-allCharacters-merged.json"):
@@ -87,3 +90,57 @@ def load_starwars_igraph(file="starwars-full-interactions-allCharacters-merged.j
     G.es["weight"] = weights
 
     return G
+
+def load_plot_config():
+    # Time to plot. Chat wrote a mpl config:
+    mpl.rcParams.update({
+        # Use LaTeX for all text rendering
+        "text.usetex": True,
+
+        # Match LaTeX default font
+        "font.family": "serif",
+        "font.serif": ["Computer Modern Roman"],
+
+        # Font sizes (11pt article standard)
+        "font.size": 12,
+        "axes.titlesize": 12,
+        "axes.labelsize": 12,
+        "xtick.labelsize": 10,
+        "ytick.labelsize": 10,
+        "legend.fontsize": 10,
+
+        # Figure layout
+        "figure.dpi": 300,
+        "savefig.dpi": 300,
+        "figure.autolayout": False,
+
+        # Lines and markers
+        "lines.linewidth": 1.5,
+        "lines.markersize": 5,
+
+        # Axes styling
+        "axes.spines.top": False,
+        "axes.spines.right": False,
+        "axes.grid": False,
+    })
+
+    FIGSIZE_SINGLE = (3.4, 2.5)   # inches
+    FIGSIZE_DOUBLE = (6.8, 3.5)
+    FIGSIZE_MEDIUM = (1.3 * FIGSIZE_SINGLE[0], 1.3 * FIGSIZE_SINGLE[1])
+    FIGSIZE_SINGLE_SQUARE = (3.4, 3.4)
+    FIGSIZE_MEDIUM_SQUARE = (3.4 * 1.3, 3.4 * 1.3)
+
+    cmap = plt.get_cmap("tab10")
+    colors = cmap(np.linspace(0, 1, 10))
+
+    mpl.rcParams['axes.prop_cycle'] = cycler(color=colors)
+    COL = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    return {
+        "FIGSIZE_SINGLE": FIGSIZE_SINGLE,
+        "FIGSIZE_DOUBLE": FIGSIZE_DOUBLE,
+        "FIGSIZE_MEDIUM": FIGSIZE_MEDIUM,
+        "FIGSIZE_SINGLE_SQUARE": FIGSIZE_SINGLE_SQUARE,
+        "FIGSIZE_MEDIUM_SQUARE": FIGSIZE_MEDIUM_SQUARE,
+        "COL": COL
+    }
